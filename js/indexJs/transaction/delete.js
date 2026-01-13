cd5ac8cca467f8ae2fea88d9bf2aac5dbc1d2c08
mayurpanchal-12 <159720053+mayurpanchal-12@users.noreply.github.com>
2026-01-13 12:06:52 +0530

    Refactor deleteTransaction function implementation
    Refactor deleteTransaction to use splice for in-place deletion.

--- js/indexJs/transaction/delete.js
+++ js/indexJs/transaction/delete.js
@@ -1,12 +1,37 @@
+// import { renderTransactions } from "./render.js";
+
+// let transactionHistory;
+
+// export function initDeleteModule(transactionsRef) {
+//   transactionHistory = transactionsRef;
+// }
+
+// export function deleteTransaction(id) {
+//   transactionHistory = transactionHistory.filter(tx => tx.id !== id);
+//   renderTransactions();
+// }
+
+
 import { renderTransactions } from "./render.js";
 
 let transactionHistory;
 
 export function initDeleteModule(transactionsRef) {
   transactionHistory = transactionsRef;
+  console.log("dele1");
 }
 
 export function deleteTransaction(id) {
-  transactionHistory = transactionHistory.filter(tx => tx.id !== id);
+  // Find the index of the transaction to delete
+  const index = transactionHistory.findIndex(tx => tx.id === id);
+  
+  // Remove it in-place to keep the array reference intact
+  if (index !== -1) {
+    transactionHistory.splice(index, 1);
+  }
+
+  console.log("del2", transactionHistory);
+
+  // Re-render the table (renderTransactions already reads the shared array)
   renderTransactions();
 }
