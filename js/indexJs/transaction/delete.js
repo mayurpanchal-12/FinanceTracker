66d8ca1cfd7cee613a4d77cee5732b283f134b24
mayurpanchal-12 <159720053+mayurpanchal-12@users.noreply.github.com>
2026-01-13 13:18:09 +0530

    update code

--- js/indexJs/transaction/delete.js
+++ js/indexJs/transaction/delete.js
@@ -1,16 +1,3 @@
-// import { renderTransactions } from "./render.js";
-
-// let transactionHistory;
-
-// export function initDeleteModule(transactionsRef) {
-//   transactionHistory = transactionsRef;
-// }
-
-// export function deleteTransaction(id) {
-//   transactionHistory = transactionHistory.filter(tx => tx.id !== id);
-//   renderTransactions();
-// }
-
 
 import { renderTransactions } from "./render.js";
 
